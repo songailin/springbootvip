d477ae90ae763a721545e84442c0a71cffe9ee70
songailin <songailin@qq.com>
2019-04-04 15:39:03 +0800

    增加事务支持

--- src/main/java/com/easysoft/controller/UserController.java
+++ src/main/java/com/easysoft/controller/UserController.java
@@ -1,12 +1,45 @@
 package com.easysoft.controller;
 
+import com.easysoft.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @Autowired
+    private IUserService userService;
+
     @RequestMapping("/hello")
     public String sayHello() {
         return "hello";
     }
+
+    @RequestMapping("/login")
+    public String login(String username,String passwd) {
+        boolean login = userService.login(username, passwd);
+        if(login) {
+            return "登陆成功";
+        }else {
+            return  "登陆失败";
+        }
+    }
+
+    @RequestMapping("/register")
+    public String register(String username,String passwd) {
+        boolean login = userService.register(username, passwd);
+        if(login) {
+            return "注册成功";
+        }else {
+            return  "注册失败";
+        }
+    }
+
+    @RequestMapping("/batchAdd")
+    public String batchAdd(String username,String passwd) {
+        userService.batchAdd(username, passwd);
+        return "成功";
+    }
+
+
 }
